87eea8f6910d3ada12700b93c65f8386cb693e6a
Hakoen <0949698@hr.nl>
2018-06-14 16:03:15 +0200

    Register systeem backend gemaakt!!!!!!!

--- Rentalia/Rentalia/FieldCheckerMethod.cs
+++ Rentalia/Rentalia/FieldCheckerMethod.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Rentalia.Data;
+using Xamarin.Forms;
 
 namespace Rentalia
 {
-    public class FieldCheckerMethod
+    public class FieldCheckerMethod : ContentPage
     {
         public bool IsFilled(string field)
         {
@@ -18,14 +20,31 @@ namespace Rentalia
                 return false;
             }
         }
-
-        public Gebruiker AddGebruiker(string voornaam, string tussen, string achternaam, string email)
+        public bool PassEqual(string pass1, string pass2)
         {
-            if IsFilled() && IsFlled() && IsFlled()
+            if (pass1 == pass2)
+            {
+                return true;
+            }
+            else
+            {
+                DisplayAlert("Alert", "Je ingevulde wachtwoorden komen niet overeen!", "Oke");
+                return false;
+            }
+        }
+
+        public Gebruiker AddGebruiker(string voornaam, string tussen, string achternaam, string email, string pass1, string pass2)
+        {
+            if (IsFilled(email) && IsFilled(voornaam) && IsFilled(achternaam) && IsFilled(pass1) && PassEqual(pass1, pass2))
             {
                 DateTime now = DateTime.Now;
-                Gebruiker nieuweGebruiker = new Gebruiker(gcodeMETHode, voornaam, tussen, achternaam, email, now, 0, 0);
+                Gebruiker nieuweGebruiker = new Gebruiker("420691337yoMum", voornaam, tussen, achternaam, email, now, 0, 0);
                 return nieuweGebruiker;
+            }
+            else
+            {
+                DisplayAlert("Alert", "Je hebt niet alle verplichte invulvelden ingevuld!", "Oke");
+                return null;
             }
         }
     }
