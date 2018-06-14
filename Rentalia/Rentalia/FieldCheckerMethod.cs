926eacda1f8065423aca44417e4f82ef1190a756
Hakoen <0949698@hr.nl>
2018-06-14 14:49:51 +0200

    Fied cheker method toegevoegd

---
+++ Rentalia/Rentalia/FieldCheckerMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Rentalia.Data.Gebruiker;
+using static Rentalia.Data.Aanbieding;
+using static Rentalia.Data.Bericht;
+using static Rentalia.Data.Boeking;
+using static Rentalia.Data.Foto;
+
+namespace Rentalia
+{
+    public class FieldCheckerMethod
+    {
+        public bool IsFilled(string field)
+        {
+            if (field.Length > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Gebruiker addGebruiker(string voornaam, string tussen, string achternaam, string email)
+        {
+            return new Gebruiker();
+        }
+    }
+}
