df91e98a3583b3c6aa9f433e3db4795d6654821b
Hakoen <0949698@hr.nl>
2018-06-17 01:48:28 +0200

    Register & Login fix

--- Rentalia/Rentalia/RegisterPage.xaml.cs
+++ Rentalia/Rentalia/RegisterPage.xaml.cs
@@ -27,13 +27,13 @@ namespace Rentalia
             var a = new FieldCheckerMethod();
             a.AddGebruiker(voornaam.ToString(), tussen.ToString(), achternaam.ToString(), email.ToString(), pass1.ToString(), pass2.ToString());
 
-            if (a.IsFilled(email.ToString()) && a.IsFilled(voornaam.ToString()) && a.IsFilled(achternaam.ToString()) && a.IsFilled(pass1.ToString()) && a.PassEqual(pass1.ToString(), pass2.ToString()))
+            if (a.IsFilled(email.Text) && a.IsFilled(voornaam.Text) && a.IsFilled(achternaam.Text) && a.IsFilled(pass1.Text) && a.PassEqual(pass1.Text, pass2.Text))
             {
                 App.Current.MainPage = new HubPage();
             }
             else
             {
-                DisplayAlert("Alert", "Je hebt niet alle verplichte velden ingevuld!", "Oke");
+                DisplayAlert("Alert", $"Je hebt niet alle verplichte velden ingevuld!{email}{voornaam}{achternaam}{pass1}{pass2}", "Oke");
             }
         }
 
