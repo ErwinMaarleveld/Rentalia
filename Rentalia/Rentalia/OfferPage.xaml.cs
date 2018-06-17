d4a52a2583f566b2d644c290899d978246076b52
Noot015 <0957904@hr.nl>
2018-06-17 06:35:01 +0200

    zooi

--- Rentalia/Rentalia/OfferPage.xaml.cs
+++ Rentalia/Rentalia/OfferPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -40,4 +41,34 @@ namespace Rentalia
         }
 
     }
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public ObservableCollection<string> Items { get; set; }
+
+    public OfferList()
+    {
+        InitializeComponent();
+
+        Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+
+        MyListView.ItemsSource = Items;
+    }
+
+    async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        if (e.Item == null)
+            return;
+
+        await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+        //Deselect Item
+        ((ListView)sender).SelectedItem = null;
+    }
+}
 }
