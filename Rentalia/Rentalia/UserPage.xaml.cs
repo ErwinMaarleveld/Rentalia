eaee2a01e375b2be8a79be07c112726e4425ee7d
Hakoen <0949698@hr.nl>
2018-06-18 18:45:05 +0200

    Alle data van een gebruiker nu in een variabel gezet te gebruiker voor profiel pagina.

--- Rentalia/Rentalia/UserPage.xaml.cs
+++ Rentalia/Rentalia/UserPage.xaml.cs
@@ -15,9 +15,11 @@ namespace Rentalia
 		public UserPage ()
 		{
 			InitializeComponent ();
-            Gebruiker SelectedGebruiker = new Gebruiker("G0001", "Handige", "", "Harry", "handigehardeharry@gmail.com", DateTime.Now, 4.3423f, 27);
+            Gebruiker SelectedGebruiker = new Gebruiker("G0001", "Handige", "", "Harry", "handigehardeharry@gmail.com", DateTime.Now, 4.3423f, 27); //De gebruiker die overeen komt met de Gcode van de gebruiker waarop je hebt geklikt.
             string profielnaam = $"{SelectedGebruiker.Voornaam} {SelectedGebruiker.Tussenvoegsel} {SelectedGebruiker.Achternaam}";
+            string profielsinds = SelectedGebruiker.LidGeworden.Year.ToString();
             float profielrating = SelectedGebruiker.Rating;
+            int profielaantalrating = SelectedGebruiker.AantalRatings;
             //string profielfoto = Path naar profiel foto in database
 
         }
