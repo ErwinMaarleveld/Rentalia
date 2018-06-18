ba73b35ed079332bdad06aed27d4650af7a9672b
Noot015 <0957904@hr.nl>
2018-06-18 18:36:00 +0200

    begin requestpage uitlezen items

--- Rentalia/Rentalia/RequestPage.xaml.cs
+++ Rentalia/Rentalia/RequestPage.xaml.cs
@@ -4,18 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Rentalia.Data;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Rentalia
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class RequestPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RequestPage : ContentPage
+    {
+        public string code {get;}
 		public RequestPage ()
 		{
 			InitializeComponent ();
-		}
+            Aanbieding currentAanbieding = new Aanbieding("testAanbieding", "Grasmaaier HUREN", "Goede grasmaair, zgan", 69, DateTime.Now, new Gebruiker("veld", "veld", "veld", "veld", "veld", DateTime.Now, 0, 0));
+        }
         public void OnClickMailBox()
         {
             App.Current.MainPage = new MessagePage();
